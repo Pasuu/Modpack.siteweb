fc2a0964dab3ed86fd66f594fe3acd0952c6b1da
Pasuu <3165251314@qq.com>
2023-08-21 12:51:14 +0800

    s

--- js/list.js
+++ js/list.js
@@ -12,7 +12,7 @@ function createPluginItem(packname, modpack) {
   const temp = `
   <div class="bordered">
       <div class="image-container" id="${packname}">
-        <img src="${modpack.img}" alt="${packname}" width="200" height="200">
+        <img src="https://media.forgecdn.net/avatars/${modpack.img}" alt="${packname}" width="200" height="200">
       </div>
       <div class="content" style="overflow: hidden;">
         <p id="pack-name"><strong>${packname}</strong></p>
@@ -65,7 +65,7 @@ function createPluginItem(packname, modpack) {
   }
   if (modpack["link"]["anyijun"]) {
     const anyijun = new DOMParser().parseFromString(`<a href="https://anyijun.com/" target="_blank"><img src="/images/anyijun.svg" alt="anyijun" width="22" height="22"/></a>`, "text/html");
-    anyijun.querySelector("a").style.marginRight = "1px";
+    anyijun.querySelector("a").style.marginLeft = "1px";
     links.appendChild(anyijun.querySelector("a"));
   }
   if (modpack["link"]["CFPAOrg"]) {
