f2fd0b54fbcdbce8d4dc52e0c1688cfc5894f137
Pasuu <3165251314@qq.com>
2023-08-22 11:42:02 +0800

    s

--- js/list.js
+++ js/list.js
@@ -164,3 +164,10 @@ error: function (xhr, status, error) {
 });
 
 
+window.addEventListener('DOMContentLoaded', function() {
+  var imageContainers = document.getElementsByClassName('image-container');
+  var countImageContainers = imageContainers.length;
+  
+  var countElement = document.getElementById('count');
+  countElement.textContent = countImageContainers;
+});
