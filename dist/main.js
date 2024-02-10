dd4beba20d1e6c0d172ab9b151551533ed9f756e
Pasuu <3165251314@qq.com>
2024-02-10 18:48:56 +0800

    mess

--- dist/main.js
+++ dist/main.js
@@ -86,24 +86,25 @@ function showAll() {
   }
 }  /* 搜索 */
 
-const messElement = document.getElementById("mess");
-const commentWidget = document.querySelector(".comment-widget");
+document.addEventListener("click", function(event) {
+  const target = event.target;
 
-messElement.addEventListener("click", function() {
-  const commentWidget = document.querySelector(".comment-widget"); 
+  if (target.matches(".mess img")) {
+      const commentWidget = document.querySelector(".comment-widget"); 
 
-  if (!commentWidget) {
-    console.error("评论窗口元素未找到！");
-    return;
-  }
+      if (!commentWidget) {
+          console.error("评论窗口元素未找到！");
+          return;
+      }
 
-  const computedStyle = window.getComputedStyle(commentWidget);
-  const display = computedStyle.getPropertyValue("display");
-
-  if (display === "none") {
-    commentWidget.style.display = "block";
-  } else {
-    commentWidget.style.display = "none";
+      const computedStyle = window.getComputedStyle(commentWidget);
+      const display = computedStyle.getPropertyValue("display");
+    
+      if (display === "none") {
+        commentWidget.style.display = "block";
+      } else {
+        commentWidget.style.display = "none";
+      }
   }
 }); /* 评论*/
 
