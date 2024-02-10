e54e0913d1ba1a510d2085cb5bafe3566be0d065
Pasuu <3165251314@qq.com>
2024-02-10 12:43:13 +0800

    ces

--- cc/main.js
+++ cc/main.js
@@ -86,25 +86,3 @@ function showAll() {
   }
 }  /* 搜索 */
 
-
-
-const messElement = document.getElementById("mess");
-const commentWidget = document.querySelector(".comment-widget");
-
-messElement.addEventListener("click", function() {
-  const commentWidget = document.querySelector(".comment-widget"); 
-
-  if (!commentWidget) {
-    console.error("评论窗口元素未找到！");
-    return;
-  }
-
-  const computedStyle = window.getComputedStyle(commentWidget);
-  const display = computedStyle.getPropertyValue("display");
-
-  if (display === "none") {
-    commentWidget.style.display = "block";
-  } else {
-    commentWidget.style.display = "none";
-  }
-}); /* 评论 */
