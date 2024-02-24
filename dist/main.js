889be94617280ac1970421bb41112e29a1410762
Pasuu <3165251314@qq.com>
2024-02-25 01:59:51 +0800

    mess

--- dist/main.js
+++ dist/main.js
@@ -56,26 +56,27 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })(); /* 百度统计 */
 
-const messElement = document.getElementById("mess");
+document.addEventListener("DOMContentLoaded", function() {
+  const messElement = document.getElementById("mess");
   const commentWidget = document.querySelector(".comment-widget");
 
   messElement.addEventListener("click", function() {
-    const commentWidget = document.querySelector(".comment-widget"); 
+      if (!commentWidget) {
+          console.error("评论窗口元素未找到！");
+          return;
+      }
 
-    if (!commentWidget) {
-      console.error("评论窗口元素未找到！");
-      return;
-    }
+      const computedStyle = window.getComputedStyle(commentWidget);
+      const display = computedStyle.getPropertyValue("display");
 
-    const computedStyle = window.getComputedStyle(commentWidget);
-    const display = computedStyle.getPropertyValue("display");
-
-    if (display === "none") {
-      commentWidget.style.display = "block";
-    } else {
-      commentWidget.style.display = "none";
-    }
-  }); /* 评论*/
+      if (display === "none") {
+          commentWidget.style.display = "block";
+      } else {
+          commentWidget.style.display = "none";
+      }
+  });
+});
+/* 评论*/
 
 new Valine({
   el: '#vcomments',
